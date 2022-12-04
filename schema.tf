98ea5e01a33a6ec4ed4356ce21e69281f51e5883
Sushma <sushma.gayatri451@gmail.com>
2022-12-04 16:47:12 +0530

    mysql

---
+++ schema.tf
@@ -0,0 +1,15 @@
+resource "null_resource" "cluster" {
+  depends_on = [aws_db_instance.mysql]
+  provisioner "local-exec" {
+    command = <<EOF
+   cd /tmp/
+   curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
+   unzip mysql-main
+   cd mysql-main
+   mysql -h ${aws_db_instance.mysql.address}  -uadmin1 -pRoboShop1 < shipping.sql
+  EOF
+  }
+}
+
+# Install Mysql on the jenkins node
+# Check on the jenkins node if the schema is loaded using the command mysql -h the end point that comes up -uadmin1 -pRoboShop1
